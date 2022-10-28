d538b18406bcb34e899d2834e1e28ef9f65a4a92
Jeff Lobacz <jeff.lobacz@gmail.com>
2022-10-28 12:46:04 -0700

    First commit from Pro

---
+++ main.py
@@ -0,0 +1,30 @@
+# with open("weather_data.csv") as file:
+#     data = file.readlines()
+# print(data)
+
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+# print(type(data["temp"]))
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+# sum = sum(temp_list)
+# avg = sum/len(temp_list)
+# print(avg)
+
+print(data["temp"].max())
